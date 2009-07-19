c9740c8ef223ff89a08b6e6a5e601019080ecc80
babak.mozaffari <babak.mozaffari@0d1bb55c-3c09-11de-be05-5f7a86268029>
2009-07-19 09:55:29 +0000

    bug fix

--- packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/BackendFileManager.java
+++ packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/BackendFileManager.java
@@ -1,16 +1,12 @@
 package org.codeandroid.vpnc_frontend;
 
-import android.app.Activity;
-
-import java.io.File;
-
-import android.os.Bundle;
-import android.util.Log;
-
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 // This class manages the creation of files on the disk in the /data/data/ direcotry.
 public class BackendFileManager extends Activity
@@ -21,8 +17,6 @@ public class BackendFileManager extends Activity
 	// FIXME: make symlinks for the route/ifconfig to bb portably/sanely.
 	// Ideally i'd like to iterate through the list, but can't see an easy way 
 	private static String[] files = {"vpnc", "vpnc-script", "bb", "make-tun-device"};
-
-	private static String targetDirectory = "/data/data/org.codeandroid.vpnc_frontend/files/";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -35,7 +29,7 @@ public class BackendFileManager extends Activity
 
 			for( int i = 0; i < files.length; i++ )
 			{
-				copyFromAsset( files[i], new File( targetDirectory + files[i] ) );
+				copyFromAsset( files[i] );
 			}
 
 		}
@@ -47,13 +41,13 @@ public class BackendFileManager extends Activity
 		finish();
 	}
 
-	void copyFromAsset(String srcName, File dst) throws IOException
+	void copyFromAsset(String fileName) throws IOException
 	{
 
-		Log.i( LOG_TAG, "Copying " + srcName + " to " + dst );
+		Log.i( LOG_TAG, "Copying " + fileName + " to " + getFilesDir() + "/" + fileName );
 
-		InputStream in = this.getAssets().open( srcName );
-		OutputStream out = new FileOutputStream( dst );
+		InputStream in = this.getAssets().open( fileName );
+		OutputStream out = openFileOutput( fileName, MODE_PRIVATE );
 
 		// this should be fine for size ?
 		byte[] buf = new byte[10240];
