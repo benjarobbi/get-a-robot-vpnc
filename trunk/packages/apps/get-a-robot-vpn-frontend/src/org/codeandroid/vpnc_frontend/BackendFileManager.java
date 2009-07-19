e23dd94e7dd6d51fa75f63ee2d1977a8f4855986
wmealing <wmealing@0d1bb55c-3c09-11de-be05-5f7a86268029>
2009-07-19 18:44:15 +0000

    Set permissions up for files by wildcard. make symlinks for route, ifconfig Don't copy files there if they already exist (for faster start-up).

--- trunk/packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/BackendFileManager.java
+++ trunk/packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/BackendFileManager.java
@@ -3,6 +3,9 @@ package org.codeandroid.vpnc_frontend;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.File;
+import java.lang.Process; 
+import java.io.DataOutputStream; 
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -26,7 +29,6 @@ public class BackendFileManager extends Activity
 
 		try
 		{
-
 			for( int i = 0; i < files.length; i++ )
 			{
 				copyFromAsset( files[i] );
@@ -38,11 +40,39 @@ public class BackendFileManager extends Activity
 			Log.e( LOG_TAG, "Exception copying asset", t );
 		}
 
+		try 
+		{
+			symlinkFile("bb", "ifconfig");
+			symlinkFile("bb", "route"); 
+
+		}
+		catch ( Throwable t ) 
+		{
+			Log.e(LOG_TAG, "Exception attempting to create symlink", t );
+		}
+
+
+		try {
+			setFilesExecutable(); 
+		}
+		catch (Throwable t) {
+			Log.e(LOG_TAG, "Exception attempting to set files executable", t);
+		}
+
 		finish();
 	}
 
 	void copyFromAsset(String fileName) throws IOException
 	{
+
+
+                File dstfile = new File( getFilesDir() + "/" + fileName ); 
+ 
+                if (dstfile.exists()) { 
+                        Log.i(LOG_TAG, "File exists, not copying"); 
+			return; 
+                } 
+
 
 		Log.i( LOG_TAG, "Copying " + fileName + " to " + getFilesDir() + "/" + fileName );
 
@@ -61,4 +91,47 @@ public class BackendFileManager extends Activity
 		in.close();
 		out.close();
 	}
+
+	private void symlinkFile(String res, String lnk) throws IOException {
+
+
+		File linkfil = new File(lnk);
+
+		if (linkfil.exists()) {
+			Log.i(LOG_TAG, "File exists, ninja");
+		}
+
+		try {
+			Process process = Runtime.getRuntime().exec("sh");
+			DataOutputStream out = new DataOutputStream(process.getOutputStream());
+
+			Log.i(LOG_TAG, "ln -s " + getFilesDir() + "/" +  res + " " + getFilesDir() + "/" +  lnk + "\n");
+			out.writeBytes("ln -s " + getFilesDir() + "/" + res + " " + getFilesDir() + "/" +  lnk + "\n");
+			out.writeBytes("exit\n");
+			out.flush();
+		}
+		catch (IOException e) {
+			Log.i(LOG_TAG, "Problem making symbolic link, what ever will we do"); 
+		}
+	}
+
+	private void setFilesExecutable() throws IOException {
+
+		try {
+			Process process = Runtime.getRuntime().exec("sh");
+			DataOutputStream out = new DataOutputStream(process.getOutputStream());
+			out.writeBytes("chmod 755 " + getFilesDir() + "/*\n");
+			out.writeBytes("exit\n");
+			out.flush();
+	
+		}
+		catch (IOException e) {
+			Log.i(LOG_TAG, "Problem making symbolic link, what ever will we do"); 
+		}	
+
+
+		Log.i(LOG_TAG, "Done setting permission");
+
+	}
+
 }
