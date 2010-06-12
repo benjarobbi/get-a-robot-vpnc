a6370316364226df4d456ed7c343aa756492a399
babak.mozaffari <babak.mozaffari@0d1bb55c-3c09-11de-be05-5f7a86268029>
2010-06-12 20:10:18 +0000

    Issue 81: PS may return PID in different columns depending on PS version

--- trunk/packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/Util.java
+++ trunk/packages/apps/get-a-robot-vpn-frontend/src/org/codeandroid/vpnc_frontend/Util.java
@@ -4,23 +4,76 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 import android.util.Log;
 
-
 public class Util
 {
+
 	private static final String LOG_TAG = "VPN_Connections";
 	public static final int DISCONNECT_NOTIFICATION = 1;
-	
-	public static int getProcessId()
+	private static final int pidColumn = getPidColumn();
+
+	public static int getPidColumn()
 	{
 		try
 		{
 			Process psProcess = Runtime.getRuntime().exec( "sh" );
 			OutputStream os = psProcess.getOutputStream();
 			InputStream is = psProcess.getInputStream();
-			writeLine( os, null, "ps | grep 'vpnc$' | cut -c 10-14" );
+			writeLine( os, null, "ps | grep PID" );
+			writeLine( os, null, "exit" );
+			try
+			{
+				psProcess.waitFor();
+			}
+			catch( InterruptedException interruptedException )
+			{
+				Log.e( LOG_TAG, "While trying to read process id", interruptedException );
+				return -1;
+			}
+			String headerLine = readString( is, null, false );
+			Log.d( LOG_TAG, "Read PS header line as " + headerLine );
+			if( headerLine == null || headerLine.trim().length() == 0 )
+			{
+				Log.e( LOG_TAG, "Attempt to do a PS did not return anything" );
+				return -1;
+			}
+			else
+			{
+				StringTokenizer tokenizer = new StringTokenizer( headerLine, " ", false );
+				int columnCount = tokenizer.countTokens();
+				for( int index = 0; index < columnCount; index++ )
+				{
+					if( "PID".equals( tokenizer.nextToken() ) )
+					{
+						Log.d( LOG_TAG, "PID is in column #" + index );
+						return index;
+					}
+				}
+				return -1;
+			}
+		}
+		catch( IOException e )
+		{
+			Log.e( LOG_TAG, "While trying to read process id", e );
+			return -1;
+		}
+	}
+
+	public static int getProcessId()
+	{
+		if( pidColumn == -1 )
+		{
+			return -1;
+		}
+		try
+		{
+			Process psProcess = Runtime.getRuntime().exec( "sh" );
+			OutputStream os = psProcess.getOutputStream();
+			InputStream is = psProcess.getInputStream();
+			writeLine( os, null, "ps | grep 'vpnc$'" );
 			writeLine( os, null, "exit" );
 			try
 			{
@@ -31,19 +84,25 @@ public class Util
 				Log.e( LOG_TAG, "While trying to read process id", interruptedException );
 				return 0;
 			}
-			String pidString = readString(is, null, false);
-			if( pidString == null || pidString.trim().length() == 0 )
+			String pidStringLine = readString( is, null, false );
+			Log.d( LOG_TAG, "Read vpnc process line as " + pidStringLine );
+			if( pidStringLine == null || pidStringLine.trim().length() == 0 )
 			{
 				Log.d( LOG_TAG, "Attempt to read vpnc process id did not return anything" );
 				return -1;
 			}
 			else
 			{
-				pidString = pidString.trim();
+				StringTokenizer tokenizer = new StringTokenizer( pidStringLine, " ", false );
+				String pidString = tokenizer.nextToken();
+				for( int index = 0; index < pidColumn; index++ )
+				{
+					pidString = tokenizer.nextToken();
+				}
 				Log.d( LOG_TAG, "Read vpnc process id as " + pidString );
 				try
 				{
-					return Integer.parseInt(pidString);
+					return Integer.parseInt( pidString );
 				}
 				catch( NumberFormatException e )
 				{
@@ -59,7 +118,7 @@ public class Util
 		}
 	}
 
-	public static String readString( InputStream is, PrintWriter logWriter, boolean block ) throws IOException
+	public static String readString(InputStream is, PrintWriter logWriter, boolean block) throws IOException
 	{
 		if( !block && is.available() == 0 )
 		{
@@ -74,7 +133,7 @@ public class Util
 		String string = new String( characters );
 		if( logWriter != null )
 		{
-			logWriter.println(string);
+			logWriter.println( string );
 		}
 		return string;
 	}
@@ -85,51 +144,51 @@ public class Util
 		os.write( line.getBytes() );
 		if( logWriter != null )
 		{
-			logWriter.println(value);
+			logWriter.println( value );
 		}
 	}
-	
-	public static void debug( String msg )
+
+	public static void debug(String msg)
 	{
 		Log.d( LOG_TAG, msg );
 	}
-	
-	public static void debug( String msg, Throwable throwable )
+
+	public static void debug(String msg, Throwable throwable)
 	{
 		Log.d( LOG_TAG, msg, throwable );
 	}
-	
-	public static void info( String msg )
+
+	public static void info(String msg)
 	{
 		Log.i( LOG_TAG, msg );
 	}
-	
-	public static void info( String msg, Throwable throwable )
+
+	public static void info(String msg, Throwable throwable)
 	{
 		Log.i( LOG_TAG, msg, throwable );
 	}
-	
-	public static void warn( String msg )
+
+	public static void warn(String msg)
 	{
 		Log.w( LOG_TAG, msg );
 	}
-	
-	public static void warn( String msg, Throwable throwable )
+
+	public static void warn(String msg, Throwable throwable)
 	{
 		Log.w( LOG_TAG, msg, throwable );
 	}
-	
-	public static void error( String msg )
+
+	public static void error(String msg)
 	{
 		Log.e( LOG_TAG, msg );
 	}
-	
-	public static void error( String msg, Throwable throwable )
+
+	public static void error(String msg, Throwable throwable)
 	{
 		Log.e( LOG_TAG, msg, throwable );
 	}
-	
-	public static void printLog( int priority, String msg )
+
+	public static void printLog(int priority, String msg)
 	{
 		Log.println( priority, LOG_TAG, msg );
 	}
